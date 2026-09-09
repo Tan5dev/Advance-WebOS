ca16a4a3c77b5fff91ef23f06267ede74be60eff
Tan5dev <tanmaypathe4@gmail.com>
2026-09-09 11:29:39 +0530

    Initial WebOS project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#1a1a2e',
+        color: 'white',
+        fontFamily: 'sans-serif',
+        fontSize: '24px',
+      }}
+    >
+      WebOS loading...
+    </div>
+  )
+}
+
+export default App
